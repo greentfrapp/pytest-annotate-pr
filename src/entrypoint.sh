ad3bf16fbe55be45a77d38e0614a2346a031f4f6
Lim Swee Kiat <limsweekiat@gmail.com>
2025-03-20 23:30:26 +0800

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -17,18 +17,19 @@ fi
 
 
 main() {
-    curl -sSL -H  "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GITHUB_TOKEN}" \
-    "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls?head=${GITHUB_REPOSITORY_OWNER}:${GITHUB_REF##*/}" > pull_request.json
+    # curl -sSL -H  "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GITHUB_TOKEN}" \
+    # "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls?head=${GITHUB_REPOSITORY_OWNER}:${GITHUB_REF##*/}" > pull_request.json
 
-    PR_BASE_SHA=$(jq -r '.[0].base.sha' < pull_request.json)
-    PR_HEAD_SHA=$(jq -r '.[0].head.sha' < pull_request.json)
+    # PR_BASE_SHA=$(jq -r '.[0].base.sha' < pull_request.json)
+    # PR_HEAD_SHA=$(jq -r '.[0].head.sha' < pull_request.json)
 
-    export PR_BASE_SHA
-    export PR_HEAD_SHA
+    # export PR_BASE_SHA
+    # export PR_HEAD_SHA
 
-    changed_files=$(git diff --name-only --diff-filter=AM "$PR_BASE_SHA" "$PR_HEAD_SHA" | grep '\.py$' | tr '\n' ' ')
+    # changed_files=$(git diff --name-only --diff-filter=AM "$PR_BASE_SHA" "$PR_HEAD_SHA" | grep '\.py$' | tr '\n' ' ')
 
-    python -m coverage run -m pytest $changed_files
+    # python -m coverage run -m pytest $changed_files
+    python -m coverage run -m pytest
     python -m coverage json
     python /src/main.py
 }
